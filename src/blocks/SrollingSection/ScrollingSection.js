85d9195e6893040cd9badd48989f5673a132ad52
snz0901@gmail.com <ngbt0409>
2021-05-18 14:51:19 +0900

    CHNG: scrolling-section, a container INSIDE.

--- src/blocks/SrollingSection/ScrollingSection.js
+++ src/blocks/SrollingSection/ScrollingSection.js
@@ -13,6 +13,12 @@ export default class ScrollingSection extends Component {
 
   render() {
     return (
+      // CONTAINER-1 > THIS > CHILDREN-PROPS (CONTAINER-2 > SCROLLING-ITEMS)
+      // * CONTAINER-1 
+        // = Defines 1(the size of the Section OUTSIDE this ScrollingSection) and 2(size of this ScrollingSeciotn with 100% width and height)
+      // * CONTAINER-1 =
+        // = Desines the height of SCROLLED contnents 
+        // = Set Flexbox to this which defines the layout of SCROLLING-iTEMS. 
       <div className="scrolling-section">
         <div className={`scrolling-section__container scrolling-section__container_background-color_${this.props.scrollingSection__Container_BackgroundColor}} scrolling-section__container_stop-scrolling_${this.props.stopScrolling}`}>
           <div className={`scrolling-section__contents`}>
