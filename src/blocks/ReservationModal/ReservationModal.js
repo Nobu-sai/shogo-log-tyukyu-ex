690ed984205beaba9d5b6089f4364373cfa5b67e
snz0901@gmail.com <ngbt0409>
2021-06-03 00:08:03 +0900

    ADD: form validation using Formik and Yup

--- src/blocks/ReservationModal/ReservationModal.js
+++ src/blocks/ReservationModal/ReservationModal.js
@@ -9,13 +9,17 @@ import React, { Component } from 'react'
 import $ from 'jquery';
 import Flatpickr from "react-flatpickr";
 import "flatpickr/dist/themes/material_blue.css";
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { date } from 'yup/lib/locale';
 
 export default class ReservationModal extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      date: new Date()
+      // date: new Date()
+      date: "日時を選択してください",
     }
 
     this.openModal = this.openModal.bind(this); 
@@ -26,6 +30,7 @@ export default class ReservationModal extends Component {
 
   componentDidMount() {
     this.$reservationModalContents = $(this.reservationModalContents);    
+    this.$reservationModalForm = $(this.reservationModalForm);    
     this.$reservationModalContents.fadeOut(0);
     setTimeout(this.openModal, 100)
     
@@ -44,9 +49,30 @@ export default class ReservationModal extends Component {
     });
   }
 
-  submitForm() {
-    console.log("submitted");
+  submitForm(values) {    
+    console.log(values)
+
+    // let formData = this.$reservationModalForm.serialize();
+    // let formData = new FormData(this.$reservationModalForm.current);
+    // console.log(formData)
+    // fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLSd36DeQOrzgKrkSuYeJd1VZhjU8eZtvtO3Zzj7EeTZaLqPRKA/formRespons", 
+    // {
+    //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    //   mode: 'no-cors', // no-cors, *cors, same-origin
+    //   // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //   // credentials: 'same-origin', // include, *same-origin, omit
+    //   headers: {
+    //     // 'Content-Type': 'application/json'
+    //     // 'Content-Type': 'application/x-www-form-urlencoded',
+    //   },
+    //   // body: formData,
+    // }
+    // )
+    // .then(data => {
+    //   console.log(data); // JSON data parsed by `data.json()` call
+    // });
     
+
     this.closeModal()
 
   }
@@ -55,92 +81,261 @@ export default class ReservationModal extends Component {
   
 
   render() {
+    
         
     return (
+// const ReservationModal = () => (
       <div className={`reservation-modal`}>
         
           <div 
             className="reservation-modal__contents reservation-modal_opacity-04"
-            ref={reservationModalContents => this.reservationModalContents = reservationModalContents} 
+            // ref={reservationModalContents => this.reservationModalContents = reservationModalContents} 
           >
+            <div className="reservation-modal__title">
+              宿泊予約
+            </div>
             <div 
               className="reservation-modal__form reservation-modal_opacity-01"
-            >
+            >            
               <div 
                 className="reservation-modal__close-icon"
-                onClick={() => this.closeModal()}
-              >
+                // onClick={this.closeModal}
+              ></div>
+                              
+              <Formik
+                initialValues={
+                  {                                         
+                    name: "",
+                    email: "",
+                    plan: "placeholder",
+                    date: "日時を選択してください", 
+                    // date: `${this.state.date}`, 
+                  }
+                }
+                validationSchema={
+                  Yup.object().shape({
+                    name: 
+                      Yup.string()
+                      // .max(15, 'Must be 15 characters or less')                    
+                      .required('必須項目になります。'),
+                    
+                    email: 
+                      Yup.string()
+                      .email('使用不可能なメールアドレスです。')
+                      .required('必須項目になります。'),
+                          
+                      
+                    plan: 
+                      Yup.mixed()
+                      .test({
+                        name: 'plan-validator',                        
+                        test: function(value) {                                                                                                 
+                          return !["①【期間限定】海辺の四季旬彩、贅沢美味懐石プラン", "② 平日に優雅に楽しむ、特別宿泊プラン","③ 絶景貸切露天と個室会席を満喫できるファミリープラン"].includes(value)
+                            // Initial situation.
+                            ? 
+                              this.createError({
+                                message: '必須項目になります。',
+                                path: 'plan', // Fieldname
+                              })
+                            : true;
+                        },
+                      }),
+                    date:                     
+                      Yup.mixed()              
+                      .test({
+                        name: 'date-validator',                        
+                        test: function(value) {                  
+                          return value === "日時を選択してください" 
+                            // Initial situation.
+                            ? 
+                              this.createError({
+                                message: '必須項目になります。',
+                                path: 'date', // Fieldname
+                              })                                                
+                            :                               
+                              true
+                        },
+                      }),                       
+                  })                  
+                }
+                onSubmit={
+                  (values, { setSubmitting }) => 
+                    {
+                      setTimeout(() => {                  
+                        setSubmitting(false);
+                        // this.submitForm(JSON.stringify(values, null, 2));
+                        alert(JSON.stringify(values, null, 2));
+                        this.submitForm(values);
+                      }, 400);
+                    }
+                }
                 
-              </div>
-              <div className="reservation-modal__form-contents">
-                <div className="reservation-modal__form-content reservation-modal__form-content_title">
-                  宿泊予約
-                </div>
+                className="reservation-modal__form-contents"
+                
+                
+              
+              >                    
 
-                <div className="reservation-modal__form-content reservation-modal__form-content_user-input-container reservation-modal__form-content_user-input-container_num_1">
-                  <div className="reservation-modal__form-content_user-input-title">
-                    お名前
-                  </div>
-                  <input className="reservation-modal__form-content_user-input" type="text" placeholder="例：石井正悟" />
-                </div>
-
-                <div className="reservation-modal__form-content reservation-modal__form-content_user-input-container">
-                  <div className="reservation-modal__form-content_user-input-title">
-                    メールアドレス
-                  </div>
-                  <input className="reservation-modal__form-content_user-input" type="text" placeholder="例：Example@example.com"/>
-                </div>
-
-                <div className="reservation-modal__form-content reservation-modal__form-content_user-input-container">
-                  <div className="reservation-modal__form-content_user-input-title reservation-modal__form-content_user-input-title_plan">
+                {
+                  ({touched, errors, values}) => 
                     
+                    
+                (
+
+                
+                <Form>
+
+                  {
+                    console.log(values)
+                  }
+
+                  {
+                    console.log("touched Object from Form JSX", touched)
+                  }
+
+
+                  <div className="reservation-modal__form-content reservation-modal__form-content_user-input-container">
+                    <label 
+                      htmlFor="name" 
+                      className="reservation-modal__form-content_user-input-title"
+                    >
+                      お名前
+                    </label>            
+                    <Field 
+                      name="name"
+                      type="text" 
+                      placeholder="例：石井正悟"
+                      className="reservation-modal__form-content_user-input"
+                      
+                    />
+                    <ErrorMessage 
+                      name="name"
+                    />  
+                  </div>
+          
+                  <div className="reservation-modal__form-content reservation-modal__form-content_user-input-container">
+                    <label 
+                      htmlFor="email" 
+                      className="reservation-modal__form-content_user-input-title"
+                    >
+                      メールアドレス
+                    </label>            
+                    <Field 
+                      name="email"
+                      type="email"
+                      placeholder="例：Example@example.com"
+                      className="reservation-modal__form-content_user-input"
+
+                    />
+                    <ErrorMessage 
+                      name="email" 
+                    />  
+                  </div>
+
+                  <div className="reservation-modal__form-content reservation-modal__form-content_user-input-container">
+                    <label 
+                      htmlFor="plan" 
+                      className="reservation-modal__form-content_user-input-title"
+                    >
                       ご希望プラン 
-                    <span className="reservation-modal__form-content_user-input-title_annotation"> 
-                     （空いているプランのみ表示されます）
-                    </span>
+                      <span className="reservation-modal__form-content_user-input-title_annotation"> 
+                      （空いているプランのみ表示されます）
+                      </span>
+                    </label>            
+                    <Field 
+                      name="plan"
+                      as="select"
+                      className="reservation-modal__form-content_user-input reservation-modal__form-content_select-box"
+                      // defaultValue="placeholder"
+                    >
+                      <option 
+                        value="placeholder" 
+                        disabled={true} 
+                        className="reservation-modal__form-content_select-box_option_placeholder"
+                      >
+                        プランを選択してください
+                      </option>
+                      <option value="①【期間限定】海辺の四季旬彩、贅沢美味懐石プラン">①【期間限定】海辺の四季旬彩、贅沢美味懐石プラン</option>
+                      <option value="② 平日に優雅に楽しむ、特別宿泊プラン">② 平日に優雅に楽しむ、特別宿泊プラン</option>
+                      <option value="③ 絶景貸切露天と個室会席を満喫できるファミリープラン">③ 絶景貸切露天と個室会席を満喫できるファミリープラン</option>
+                    </Field>      
+                    <ErrorMessage 
+                      name="plan"                      
+                    />  
                   </div>
-                  <select 
-                    name="plans"
-                    className="reservation-modal__form-content_user-input reservation-modal__form-content_select-box"
-                  >
-                    {/* プランを選択してください */}
-                    <option selected="selected" disabled="true" className="reservation-modal__form-content_select-box_option_placeholder">プランを選択してください</option>
-                    <option value="plan-1">①【期間限定】海辺の四季旬彩、贅沢美味懐石プラン</option>
-                    <option value="plan-2">② 平日に優雅に楽しむ、特別宿泊プラン</option>
-                    <option value="plan-3">③ 絶景貸切露天と個室会席を満喫できるファミリープラン </option>
-                  </select>
-                </div>
 
-                <div className="reservation-modal__form-content reservation-modal__form-content_user-input-container reservation-modal__form-content_flatpickr-container">
-                  <div className="reservation-modal__form-content_user-input-title">
-                    日時選択
+                  <div className="reservation-modal__form-content reservation-modal__form-content_user-input-container">
+                    <label 
+                      htmlFor="date" 
+                      className="reservation-modal__form-content_user-input-title"
+                    >
+                      日時選択
+                    </label>
+                    <Field 
+                      name="date"                             
+                      className="reservation-modal__form-content_user-input reservation-modal__form-content_flatpickr"        
+                    >     
+                       {({
+                          field, // { name, value, onChange, onBlur }
+                          form: { touched, errors, values, setFieldValue }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+                          meta,
+                        }) => (
+                          <div>
+                            <Flatpickr
+                            
+                              options={{ 
+                                minDate: this.state.date,
+                                mode: "range",                       
+                              }}
+                              placeholder="日時を選択してください"
+                              
+                              onChange={chosenDate => {   
+                                // console.log("touched Object from Flatpickr/onChange()", touched)
+                                setFieldValue("date", chosenDate)
+                              }}          
+                              onReady={()=> {
+                                // console.log("Formik/values from Flatpickr/onReady Prop", values)
+                              }}
+                              onValueUpdate={(params)=> {
+                                // console.log("Formik/values from Flatpickr/onValueUpdate Prop", values)
+                                // console.log("Formik/touched from Flatpickr/onValueUpdate Prop", touched)
+                              }}
+                              onDestroy={(params)=> {
+                                // console.log("Flatpickr/onDestroy()/Params", params)
+                              }}
+                              
+                              
+                            
+                                    
+                            />
+                                                    
+                          </div>
+                        )}
+                    </Field>
+                                       
+                   
+                      <ErrorMessage name="date" 
+                      />  
+                   
+                                          
                   </div>
-                  <Flatpickr 
-                    className="reservation-modal__form-content_user-input reservation-modal__form-content_flatpickr"                    
-                    options={{ 
-                      minDate: this.state.date,
-                      mode: "range",                       
-                    }}
-                    defaultValue="日時を選択してください"
-                    onChange={date => {
-                      this.setState({ date });
-                    }}
+
+                  <div className="reservation-modal__form-content reservation-modal__form-content_submit-button-container">
+                    <button
+                      type="submit"  
+                      className="reservation-modal__form-content_submit-button"
+                    >
+                      送信する
+                    </button>                  
                     
-                  /> 
-                </div>
-
-                <div className="reservation-modal__form-content reservation-modal__form-content_submit-button-container">
-                  <div 
-                    className="reservation-modal__form-content_submit-button"
-                    onClick={this.submitForm}
-                  >
-                    送信する
                   </div>
-                  
-                </div>
 
-              </div>
-            
+                </Form>       
+                
+                )}
+
+              
+              </Formik>
             </div>
           </div>
 
@@ -148,3 +343,8 @@ export default class ReservationModal extends Component {
     )
   }
 }
+
+
+// )
+                    
+// export default ReservationModal;
