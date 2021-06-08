4816ffb6382ffa43d59c4a1427243494c7c38004
snz0901@gmail.com <ngbt0409>
2021-06-09 00:23:48 +0900

    CHNG: reservation-modal__success-message text

--- src/blocks/ReservationModal/__SuccessMessage/ReservationModal__SuccessMessage.js
+++ src/blocks/ReservationModal/__SuccessMessage/ReservationModal__SuccessMessage.js
@@ -77,7 +77,7 @@ export default class ReservationModalSuccessMessage extends Component {
               <div className="reservation-modal__title reservation-modal__success-message_farewell-message">
                 {this.props.submissionContents.name}様、
                 <span>お越しをお待ち致しております。</span>
-                <span className="reservation-modal__title_annotation reservation-modal__success-message_farewell-message_annotation">ー　石井花壇スタッフ一同　ー</span>
+                <span className="reservation-modal__title_annotation reservation-modal__success-message_farewell-message_annotation">ー　石井花壇一同　ー</span>
               </div>
 
               <div className="reservation-modal__button-container">
