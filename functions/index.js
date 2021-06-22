3792b4fcc26706d5585e80f632da292d2a53cc68
snz0901@gmail.com <ngbt0409>
2021-06-22 22:08:47 +0900

    CASE: success in localhost via cloud function

--- functions/index.js
+++ functions/index.js
@@ -9,12 +9,15 @@ const app = express();
 app.use(basicAuth('user','password'));
 
 
-app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.static(path.join(__dirname, 'test-site', 'build')));
+app.use(express.static(path.join(__dirname, 'main-site', 'build')));
 
-console
 
 app.use((req, res, next) => {   
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    // res.sendFile(path.join(__dirname, '../build/index.html'));
+    // res.sendFile(path.join(__dirname, 'test-site', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'main-site', 'build', 'index.html'));
     // res.send("Site from Cloud Function/app")
 })
 
