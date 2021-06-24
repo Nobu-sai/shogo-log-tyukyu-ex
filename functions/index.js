63d8101d6eac0e2c29da73c59d731a4fcece8d8e
snz0901@gmail.com <ngbt0409>
2021-06-24 20:33:05 +0900

    fail

--- functions/index.js
+++ functions/index.js
@@ -11,12 +11,14 @@ app.use(basicAuth('user','password'));
 
 // app.use(express.static(path.join(__dirname, '../build')));
 // app.use(express.static(path.join(__dirname, 'test-site', 'build')));
+// app.use(express.static(path.join(__dirname, 'main-site', 'build')));
 app.use(express.static(path.join(__dirname, '../main-site/build')));
 
 
 app.use((req, res, next) => {   
     // res.sendFile(path.join(__dirname, '../build/index.html'));
     // res.sendFile(path.join(__dirname, 'test-site', 'build', 'index.html'));
+    // res.sendFile(path.join(__dirname, 'main-site', 'build', 'index.html'));
     res.sendFile(path.join(__dirname, '../main-site/build/index.html'));
     // res.send("Site from Cloud Function/app")
 })
