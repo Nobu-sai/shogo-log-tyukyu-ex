8cb6dc62c6d5844b4b413fd7159409c8760e3555
snz0901@gmail.com <ngbt0409>
2021-07-02 23:06:22 +0900

    CHNG: Node Server, Port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 30000;
 const path = require("path");
 const fs = require("fs");
 
